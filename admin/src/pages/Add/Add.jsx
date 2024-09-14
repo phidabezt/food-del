0bb6f6b333cb3d26b04fb753ee55849b4c965c44
Phi Nguyen <quangphi1231@gmail.com>
2024-09-14 22:19:03 +0700

    init pages

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,9 @@
+import './Add.scss'
+
+const Add = () => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default Add
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,9 @@
+import './Orders.scss'
+
+const Orders = () => {
+  return (
+    <div>Orders</div>
+  )
+}
+
+export default Orders
