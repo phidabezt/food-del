118aa757cc1aa367b740b7e05079052ac8b35d70
Phi Nguyen <quangphi1231@gmail.com>
2024-09-18 17:19:01 +0700

    finish the logic of my orders

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import { assets } from '../../assets/assets'
+import './MyOrders.scss'
+
+const MyOrders = () => {
+  const { url, token } = useContext(StoreContext)
+  const [data, setData] = useState([])
+
+  const fetchOrders = async () => {
+    const response = await axios.post(
+      url + '/api/order/userorders',
+      {},
+      {
+        headers: { token },
+      }
+    )
+    setData(response.data.data)
+  }
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders()
+    }
+  }, [token])
+
+  return (
+    <div className='orders'>
+      <h2>My Orders</h2>
+      <div className='orders__container'>
+        {data.map((order, index) => {
+          return (
+            <div key={index} className='orders__order'>
+              <img src={assets.parcel_icon} alt='' />
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + ' x ' + item.quantity
+                  } else {
+                    return item.name + ' x ' + item.quantity + ', '
+                  }
+                })}
+              </p>
+              <p>${order.amount}.00</p>
+              <p>Items: {order.items.length}</p>
+              <p>
+                <span>&#x25cf;</span> <b>{order.status}</b>
+              </p>
+              <button>Track Order</button>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default MyOrders
